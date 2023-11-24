efdaf2c690f35bb9e6171ee5b491ee11a7560f5e
interrrp <int@addictedto.coffee>
2023-11-24 17:07:28 +0800

    Move Windows card before universal

--- app/page.tsx
+++ app/page.tsx
@@ -11,17 +11,17 @@ export default function Home() {
 
       <div className="flex flex-wrap justify-center gap-4">
         <DownloadCard
-          name="Universal (JAR)"
-          icon={PackageIcon}
-          downloadURL="https://skmedix.pl/binaries/_/SKlauncher-3.2.jar"
-          sha256Digest="f7d3e01dcfc001cc80a988c518d4358955842d140054214d1367972c5c543350"
-        />
-
-        <DownloadCard
           name="Windows (64-bit)"
           icon={Grid2X2Icon}
           downloadURL="https://skmedix.pl/binaries/_/x64/SKlauncher-3.2.exe"
           sha256Digest="05ae2f0dd61ef10019b94c200e8df192b767bb4cc24a7e7b329ab43cc9c74caf"
+        />
+
+        <DownloadCard
+          name="Universal (MacOS/Linux) (JAR)"
+          icon={PackageIcon}
+          downloadURL="https://skmedix.pl/binaries/_/SKlauncher-3.2.jar"
+          sha256Digest="f7d3e01dcfc001cc80a988c518d4358955842d140054214d1367972c5c543350"
         />
       </div>
     </main>
